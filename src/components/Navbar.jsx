f8c840ead00d2d103be8af3924a457d013d4b65c
Mohammed Saad Mirza <mirzamohdsaad@gmail.com>
2026-04-07 15:31:20 +0530

    Update Navbar logo to Mohammad Saad

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import './Navbar.css';
 const Navbar = ({ toggleTheme, currentTheme }) => {
   return (
     <nav className="navbar">
-      <div className="nav-container">
-        <Link to="/" className="nav-logo">Mirza</Link>
+      <div className="container nav-container">
+        <Link to="/" className="nav-logo">Mohammad Saad</Link>
         <ul className="nav-links">
           <li><Link to="/" className="nav-link">Home</Link></li>
           <li><Link to="/skills" className="nav-link">Skills</Link></li>
